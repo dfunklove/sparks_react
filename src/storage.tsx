556892ca9865212f62513e31b422e75d7a221c21
Daniel Lovette <5632199+dfunklove@users.noreply.github.com>
2023-02-14 22:00:33 -0600

    Only parse the user if it exists.

--- src/storage.tsx
+++ src/storage.tsx
@@ -10,6 +10,9 @@ export const deleteFlash = () => localStorage.removeItem(AUTH_TOKEN);
 export const getToken = () => localStorage.getItem(AUTH_TOKEN);
 export const setToken = (token: string) => localStorage.setItem(AUTH_TOKEN, token);
 export const deleteToken = () => localStorage.removeItem(AUTH_TOKEN);
-export const getUser = () => JSON.parse(localStorage.getItem(USER) || "");
+export const getUser = () => { 
+  let user = localStorage.getItem(USER);
+  return user ? JSON.parse(user) : null;
+}
 export const setUser = (user: User) => localStorage.setItem(USER, JSON.stringify(user));
 export const deleteUser = () => localStorage.removeItem(USER);
