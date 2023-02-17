2900966cf188dc11a9ac725faa0a833458970642
Daniel Lovette <5632199+dfunklove@users.noreply.github.com>
2023-02-17 14:58:38 -0600

    Show or hide lessons based on state.

--- src/components/GroupLessonDisplay.tsx
+++ src/components/GroupLessonDisplay.tsx
@@ -1,39 +1,19 @@
+import { useState } from 'react';
+import { GroupLesson } from '../graphql/generated'
 import LessonDisplay from './LessonDisplay';
-import { GroupLesson, Lesson } from '../graphql/generated'
 
 type Props = {
   group_lesson: GroupLesson
 }
 
 function GroupLessonDisplay({ group_lesson }: Props) {
-  function showGroupLesson() {
-    const toShow = document.getElementsByClassName("group-lesson-"+group_lesson.id);
-    for (let i=0; i < toShow.length; i++) {
-      (toShow[i] as HTMLElement).classList.remove("hidden")
-    }
-    const toHide = document.getElementsByClassName("expand-button-"+group_lesson.id);
-    for (let i=0; i < toHide.length; i++) {
-      (toHide[i] as HTMLElement).classList.add("hidden")
-    }
-    return null
-  }
-  function hideGroupLesson() {
-    const toShow = document.getElementsByClassName("expand-button-"+group_lesson.id);
-    for (let i=0; i < toShow.length; i++) {
-      (toShow[i] as HTMLElement).classList.remove("hidden")
-    }
-    const toHide = document.getElementsByClassName("group-lesson-"+group_lesson.id);
-    for (let i=0; i < toHide.length; i++) {
-      (toHide[i] as HTMLElement).classList.add("hidden")
-    }
-    return null
-  }
+  const [expand, setExpand] = useState(false);
 
   return <>
     <div className="tr">
       <span className="td">
-        <button className={`minimize expand-button-${group_lesson.id}`} onClick={showGroupLesson}>+</button>
-        <button className={`minimize hidden group-lesson-${group_lesson.id}`} onClick={hideGroupLesson}>-</button>
+        <button className={`minimize ${expand === true ? "hidden" : ""}`} onClick={() => {setExpand(true)}}>+</button>
+        <button className={`minimize ${expand === false ? "hidden" : ""}`} onClick={() => {setExpand(false)}}>-</button>
       </span>
       <span className="td">{group_lesson.lessonSet[0]?.school.name}</span>
       <span className="td">{group_lesson.user.firstName + " " + group_lesson.user.lastName}</span>
@@ -44,9 +24,7 @@ function GroupLessonDisplay({ group_lesson }: Props) {
       {Array(6).fill(0).map((e,i) => <span key={i} className="td"></span>)}
       <span className="td">{group_lesson.notes}</span>
     </div>
-    {group_lesson.lessonSet.map(lesson => <LessonDisplay key={lesson.id} group_lesson_id={group_lesson.id} lesson={lesson}/>)}
-    <div className={`tr group-lesson-${group_lesson.id} hidden`}>
-    </div>
+    {group_lesson.lessonSet.map(lesson => <LessonDisplay key={lesson.id} lesson={lesson} visible={expand} />)}
   </>
 }
 
